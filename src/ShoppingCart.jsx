930f3b1d609758a661a9abb11613c42937e00a65
Viktoria <doduchenko29@gmail.com>
2021-07-19 11:08:33 +0300

    add

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProductList from './ProductList';
+import ProductsList from './ProductsList';
 import CartTitle from './CartTitle';
 
 class ShoppingCart extends React.Component {
@@ -23,7 +23,7 @@ class ShoppingCart extends React.Component {
     return (
       <div className="column">
         <CartTitle userName={this.props.userName} count={count} />
-        <ProductList cartItems={this.state.cartItems}/>
+        <ProductsList cartItems={this.state.cartItems}/>
       </div>
     );
   }
