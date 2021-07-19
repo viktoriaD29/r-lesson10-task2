4088362bd6a1a6a8de5d4b4c9a96c1f550829f78
Viktoria <doduchenko29@gmail.com>
2021-07-19 11:19:08 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ShoppingCart from './ShoppingCart.jsx';
 import Profile from './Profile.jsx';
 
-class Page extends Component {
+class App extends Component {
   state = {
     userData: {
       firstName: 'Tom',
@@ -36,4 +36,4 @@ class Page extends Component {
   }
 }
 
-export default Page;
+export default App;
